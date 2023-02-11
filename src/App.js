73ca7d8b1305c8560e2e647606c11b2e7c14f13d
jayshah <jaywebdev98@gmai.com>
2023-02-11 18:56:51 +0530

    setup project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return (
+    <>
+      <h1>HELLO</h1>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
