422449d2cdeeb3bbf80d2e8b736529e8e5d595b0
jayshah <jaywebdev98@gmai.com>
2023-03-31 14:40:05 +0530

    change app logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useSearchParams } from "react-router-dom";
 import { toggleMenu, toggleMobileSearchBar } from "../utils/appSlice";
 import { caching } from "../utils/searchSlice";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -63,16 +64,9 @@ const Navbar = () => {
           </svg>
           <Link to="/">
             <div className="flex gap-x-[5px] justify-center items-center cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="#ff0000"
-                className="h-[30px]"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
-              </svg>
+              <img src={logo} className="h-[35px]" />
               <p className="font-Roboto font-semibold tracking-tight text-[20px]">
-                Youtube
+                ChillHub
               </p>
             </div>
           </Link>
